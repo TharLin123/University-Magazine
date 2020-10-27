632dd0723d6ef446f135c06f67a97661ebe538f0
TharLin123 <tharlinhtet97@gmail.com>
2020-10-27 22:23:43 +0630

    Final Versions

--- contributions/views.py
+++ contributions/views.py
@@ -320,7 +320,7 @@ def overview(request):
         student_count = Student.objects.filter(faculty=facultyy).count()
         total_comment = Comment.objects.filter(post__faculty=facultyy).count()
         without_comment = 0
-        without_cmda = contribution_count - total_comment
+        without_cmds = contribution_count - total_comment
         for contributionss in without_cmds:
             if datetime.date.today() > contributionss.date_posted.date() + timedelta(days = 14):
                 without_comment += 1
